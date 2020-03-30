2b67501ad11c393ea405f6953413177eda69f2e2
Jonathan George <Jonathan.George@endjin.com>
2020-03-30 13:26:46 +0100

    Initial solution and build/IMM/ReadMe files.

---
+++ Solutions/Marain.TenantManagement.Abstractions/Marain/TenantManagement/IMarainTenantManagementService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IMarainTenantManagementService.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.TenantManagement
+{
+    /// <summary>
+    /// Provides standard methods for managing Marain tenants.
+    /// </summary>
+    public interface IMarainTenantManagementService
+    {
+    }
+}
