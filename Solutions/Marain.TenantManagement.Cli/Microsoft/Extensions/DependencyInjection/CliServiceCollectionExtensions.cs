591397f7d30fae799632d8290dae05f591872805
Jonathan George <Jonathan.George@endjin.com>
2020-04-01 09:43:20 +0100

    WIP - started adding CLI, but had to pause due to Corvus.Tenancy mismatch between internal projects and Marain.Tenancy.

---
+++ Solutions/Marain.TenantManagement.Cli/Microsoft/Extensions/DependencyInjection/CliServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="CliServiceCollectionExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods to configure the DI container used by the CLI.
+    /// </summary>
+    public static class CliServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds services required by the command line application to the service collection.
+        /// </summary>
+        /// <param name="serviceCollection">The service collection to add to.</param>
+        /// <returns>The service collection, for chaining.</returns>
+        public static IServiceCollection AddCommandLineServices(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection;
+        }
+    }
+}
---
+++ Solutions/Marain.TenantManagement.Cli/Program.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="Program.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.TenantManagement.Cli
+{
+    using System;
+    using System.CommandLine.Builder;
+    using System.CommandLine.Hosting;
+    using System.CommandLine.Parsing;
+    using System.Threading.Tasks;
+    using Corvus.Identity.ManagedServiceIdentity.ClientAuthentication;
+    using Marain.Tenancy.Client;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// The main class for the console app.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The entry point for the program.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>When complete, an integer representing success (0) or failure (non-0).</returns>
+        public static Task<int> Main(string[] args)
+        {
+            IHostBuilder builder = Host.CreateDefaultBuilder();
+
+            builder.ConfigureServices((ctx, services) =>
+            {
+                services.AddJsonSerializerSettings();
+
+                var msiTokenSourceOptions = new AzureManagedIdentityTokenSourceOptions
+                {
+                    AzureServicesAuthConnectionString = ctx.Configuration["AzureServicesAuthConnectionString"],
+                };
+
+                services.AddAzureManagedIdentityBasedTokenSource(msiTokenSourceOptions);
+
+                var tenancyClientOptions = new TenancyClientOptions
+                {
+                    TenancyServiceBaseUri = new Uri(ctx.Configuration["TenancyClient:TenancyServiceBaseUri"]),
+                    ResourceIdForMsiAuthentication = ctx.Configuration["TenancyClient:ResourceIdForMsiAuthentication"],
+                };
+
+                services.AddSingleton(tenancyClientOptions);
+
+                services.AddTenantProviderServiceClient();
+            });
+
+            Parser parser = new CommandLineBuilder()
+                .UseDefaults()
+                .UseHost(x => builder)
+                .Build();
+
+            return parser.InvokeAsync(args);
+        }
+    }
+}
