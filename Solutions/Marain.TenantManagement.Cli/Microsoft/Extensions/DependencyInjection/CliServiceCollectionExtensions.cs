cfe8ecaf4e50756cbba36d14599beb16bb9f6449
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2025-06-13 08:30:18 +0100

    Remove unused method

--- Solutions/Marain.TenantManagement.Cli/Microsoft/Extensions/DependencyInjection/CliServiceCollectionExtensions.cs
+++ Solutions/Marain.TenantManagement.Cli/Microsoft/Extensions/DependencyInjection/CliServiceCollectionExtensions.cs
@@ -4,14 +4,9 @@
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using Corvus.Identity.ClientAuthentication.Azure;
 
 using Marain.Tenancy.Client;
-using Marain.TenantManagement.Cli.Commands;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -21,38 +16,6 @@ using Microsoft.Extensions.Logging;
 /// </summary>
 public static class CliServiceCollectionExtensions
 {
-    /// <summary>
-    /// Adds the CLI commands to the DI container. These are resolved when the commands are registered with the
-    /// Spectre.Console.Cli CommandApp.
-    /// </summary>
-    /// <param name="services">The service collection to add to.</param>
-    /// <returns>The service collection, for chaining.</returns>
-    /// <remarks>
-    /// We are using convention to register the commands; essentially everything in the same namespace as the
-    /// <see cref="InitialiseCommand"/> and that inherits from Spectre.Console.Cli Command types will be registered.
-    /// If any commands are added in other namespaces, this method will need to be modified/extended to deal with that.
-    /// </remarks>
-    public static IServiceCollection AddCliCommands(this IServiceCollection services)
-    {
-        // Register all command classes for DI resolution
-        Type initialiseType = typeof(InitialiseCommand);
-
-        IEnumerable<Type> commands = initialiseType
-            .Assembly
-            .GetExportedTypes()
-            .Where(x => x.Namespace == initialiseType.Namespace &&
-                        x.IsClass &&
-                        !x.IsAbstract &&
-                        x.Name.EndsWith("Command"));
-
-        foreach (Type command in commands)
-        {
-            services.AddTransient(command);
-        }
-
-        return services;
-    }
-
     /// <summary>
     /// Adds services required by the command line application to the service collection.
     /// </summary>
